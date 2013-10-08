b31d691907a3eada79c9f6fa8577ddc83aa61ef0
Paulo Bu <p.bu.garcia@gmail.com>
2013-10-08 10:50:37 -0700

    Adds CDN links to bootstrap libraries and adds a html file with a use case

--- confirm-modal.js
+++ confirm-modal.js
@@ -1,4 +1,32 @@
+/* ========================================================
+ * confirm-modal.js
+ * http://github.com/pbu88/confirm-modal.js
+ * ========================================================
+ *
+ * This piece of code is intended to serve as a guide 
+ * in the tedious writing of boilerplate code needed to 
+ * create confirmation dialogs in your web pages.
+ *
+ * It can be used with anchors <a/> and forms <form/> in a 
+ * declarative and easy to remember way within the tag 
+ * definition. It also can be called within a JavaScript 
+ * block of code programatically.
+ *
+ * The requirements are simple:
+ *  1. The use of jQuery (who doesn't ;) ).
+ *  2. Have a modal dialog to be called when the button is
+ *     clicked.
+ *
+ * The example here utilizes Twitter's Bootstrap library
+ * (specifically bootstrap-modal.js) to deal with modals,
+ * but the code layout is intended to be easily adapted to 
+ * any circumstance. */
+
+
 $(function(){
+
+    /* CONFIRMATION BUTTON CLASS DEFINITION
+     * ==================================== */
 
     function ConfirmationButton(element, options){
 
@@ -32,6 +60,9 @@ $(function(){
 
     }
 
+    /* CONFIRMATION BUTTON PLUGIN DEFINITION
+     * ===================================== */
+
     $.fn.confirmButton = function(option){
         return this.each(function(){
             var $this = $(this),
@@ -44,6 +75,9 @@ $(function(){
             );
         });
     };
+
+    /* CONFIRMATION BUTTON CLASS INITIALIZATION
+     * ======================================== */
 
     $('.confirm-btn').confirmButton({});
 });
